002f7da7f1d196a18a5217ec767fb018fd6ffbfe
jesse cafarelli <jesse.cafarelli@gmail.com>
2018-04-16 18:48:39 -0400

    added serverless framwork into project

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.use("/posts", indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
+const BlogStorage = require('./BlogStorage');
+const AWS = require('aws-sdk');
+
+const config = {
+    region: AWS.config.region || process.env.SERVERLESS_REGION || 'eu-west-1',
+};
+
+const dynamodb = new AWS.DynamoDB.DocumentClient(config);
+
+
+const storage = new BlogStorage(dynamodb);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    storage.getPosts({})
+        .then(response => {
+            res.json(response);
+        })
+        .catch(err => {
+            res.status(400).json({ error: err });
+        });
+});
+
+router.post('/', function(req, res, next) {
+    storage.savePost(req.body)
+        .then(function(post){
+            res.json(post)
+        })
+        .catch(function(err){
+            res.send(err)
+        })
+});
+
+router.put('/:postId', function(req, res, next) {
+    storage.updatePost(req.params.postId, req.body)
+        .then(response => next(null, response))
+        .catch(next);
+});
+
+router.delete('/:postId', function(req, res, next) {
+    storage.deletePost(req.params.postId)
+        .then(response => next(null, response))
+        .catch(next);
 });
 
 module.exports = router;
