c9ebfd647aec538e65a97205dd23c1c48fad968b
EC2 Default User <ec2-user@ip-172-31-3-128.ec2.internal>
2018-04-17 16:27:10 +0000

    minor bug fixes

--- .tools/createdb.sh
+++ .tools/createdb.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/env
+#!/usr/bin/env node
 
 const db = require('../models')
 db.dynamo.createTables(function(err) {
             if (err) {
               console.log('Error creating tables: ', err);
+              process.exit(1)
             } else {
               console.log('Tables has been created');
+              process.exit()
             }
-            exit 0;
+            process.exit(1)
           });
